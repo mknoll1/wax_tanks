f8f7a887c016e804d2a907247e197496ea269c36
mknoll <mknoll1@gmail.com>
2018-07-24 15:43:04 -0400

    adding some more model classes.  mostly just a test

--- tank_project/models.py
+++ tank_project/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
-Class WaxTank(models.Model):
+class WaxTank(models.Model):
     description = models.CharField(max_length=255)
     desired_temp = models. IntegerField(default=0)
+    sensor_id = models.CharField(max_length=255)
     
 
-Class WaxTankReading(models.Model):
+class WaxTankReading(models.Model):
     wax_tank = models.ForeignKey(WaxTank)
     timestamp = models.DateTimeField(auto_now_add=True)
     temperature = models.IntegerField(default=0)
     heat_active = models.Boolean(default=False)
 
-Class Employee(models.Model):
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email_address = models.EmailField()
+
+class WorkDay(models.Model):
+    employee = models.ForeignKey(Employee)
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTImeField()
+    break_time = models.IntegerField()
 
